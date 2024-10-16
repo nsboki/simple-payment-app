9db1d76672ce27a9b81b6097a56576ead8931450
Bojan Djordjevic <nsboki@gmail.com>
2024-10-16 23:43:57 +0200

    Created missing CRUD Payment service and controller endpoints.

--- src/main/java/com/example/payments/controller/PaymentController.java
+++ src/main/java/com/example/payments/controller/PaymentController.java
@@ -10,10 +10,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/payments")
@@ -31,4 +30,34 @@ public class PaymentController {
         PaymentDto createdPayment = paymentService.createPayment(paymentDto);
         return new ResponseEntity<>(createdPayment, HttpStatus.CREATED);
     }
+
+    @GetMapping("{id}")
+    @Operation(summary = "Get a payment", description = "Get a payment with a given id from the system")
+    public ResponseEntity<PaymentDto> getPayment(@PathVariable("id") Long paymentId) {
+        PaymentDto paymentById = paymentService.getPaymentById(paymentId);
+        return ResponseEntity.ok(paymentById);
+    }
+
+    @GetMapping
+    @Operation(summary = "Get all payments", description = "Get all payments from the system")
+    public ResponseEntity<List<PaymentDto>> getAllPayments() {
+        List<PaymentDto> allPayments = paymentService.getAllPayments();
+        return ResponseEntity.ok(allPayments);
+    }
+
+    @PutMapping("{id}")
+    @Operation(summary = "Update a payment", description = "Update a payment with a given id if exists")
+    public ResponseEntity<PaymentDto> updatePayment(@PathVariable("id") Long paymentId,
+                                                    @RequestBody PaymentDto updatedPayment) {
+        PaymentDto paymentDto = paymentService.updatePaymentById(paymentId, updatedPayment);
+        return ResponseEntity.ok(paymentDto);
+    }
+
+    @DeleteMapping("{id}")
+    @Operation(summary = "Delete a payment", description = "Delete a payment with a given id if exists")
+    public ResponseEntity<String> deletePayment(@PathVariable("id") Long paymentId) {
+        paymentService.deletePaymentById(paymentId);
+        return ResponseEntity.ok("Payment deleted successfully!");
+    }
+
 }
